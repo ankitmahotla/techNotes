2458c9e15ae0623afad04d5cfcb1466ee19ebe6a
Ankit Mahotla <ankit@rise11.com>
2023-04-04 14:55:28 +0530

    Added middleware and connected to MongoDB

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const path = require("path");
 const PORT = process.env.PORT || 3500;
+const { logger } = require("./middleware/logger");
+const errorHandler = require("./middleware/errorHandler");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
+const connectDB = require("./config/dbConn");
+const mongoose = require("mongoose");
+const { logEvents } = require("./middleware/logger");
+
+connectDB();
+
+app.use(logger);
+
+app.use(cors(corsOptions));
 
 // Allows app to Recieve and parse JSON data (Built in middleware)
 app.use(express.json());
+
+app.use(cookieParser());
 
 // Serves static files like images and css files (Built in middleware)
 app.use("/", express.static(path.join(__dirname, "public")));
@@ -24,5 +41,18 @@ app.all("*", (req, res) => {
   }
 });
 
-// Used to start the server.
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.use(errorHandler);
+
+mongoose.connection.once("open", () => {
+  console.log("Connected to MongoDB");
+  // Used to start the server.
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log(err);
+  logEvents(
+    `${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,
+    "mongoErrLog.log"
+  );
+});
